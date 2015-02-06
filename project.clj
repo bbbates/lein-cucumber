f4b21eab372b53f3690170a545106d8b469394e0
Brendan Bates <batesbrendan@gmail.com>
2015-02-06 14:32:52 +1100

    Upgrade version of cucumber-clojure

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject icm-consulting/lein-cucumber "1.0.3"
+(defproject icm-consulting/lein-cucumber "1.0.4"
   :description "Run cucumber-jvm specifications with leiningen"
-  :dependencies [[info.cukes/cucumber-clojure "1.1.6"]
+  :dependencies [[info.cukes/cucumber-clojure "1.2.2"]
                  [leiningen-core "2.0.0"]
-                 [org.clojure/clojure "1.5.0-RC3"]]
+                 [org.clojure/clojure "1.6.0"]]
   :profiles {:cucumber {:dependencies [[commons-io "2.0"]]
                         :plugins [[lein-cucumber "1.0.3"]]}}
   :eval-in :leiningen
